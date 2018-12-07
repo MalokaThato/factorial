6b6774d471c5a3de6f57909c6fc7c76c799355c6
ImmortalT1T@N <21996479+MalokaThato@users.noreply.github.com>
2018-12-07 15:56:37 +0200

    Add files via upload

---
+++ Factorial/factorial.py
@@ -0,0 +1,32 @@
+
+
+x = input("Enter the number you would like to get's factorial: ")
+
+def factorial(x):
+    result = 1
+
+    try:
+        int(x)
+
+    except:
+        return(print("Please input only numbers."))
+
+    if int(x) < 0:
+        return(print("We can not produce the factorial of a negative number:"))
+
+    else:
+
+        for p in range(1,int(x) + 1):
+            if p >= 1 or p == 0:
+
+                result = result * p
+
+
+            else:
+                continue
+
+        return(print(result))
+
+
+
+factorial(x)
